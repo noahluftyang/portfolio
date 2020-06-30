da3e07159b89fe24a18f7c00999177411f5ddb4f
Euihyun Yang <euihyun@sandboxnetwork.net>
2020-06-30 17:35:18 +0900

    feat(universal-auth-server): connect redis session

--- services/universal-auth-server/src/app.ts
+++ services/universal-auth-server/src/app.ts
@@ -1,19 +1,32 @@
-import * as bodyParser from 'body-parser';
+import { json } from 'body-parser';
 import * as compression from 'compression';
+import * as connect from 'connect-redis';
 import * as cors from 'cors';
 import * as express from 'express';
+import * as session from 'express-session';
 import * as helmet from 'helmet';
 import * as pino from 'pino-http';
+import { createClient } from 'redis';
 
 import { router } from './routes';
 
 export const app = express();
 
 const logger = pino();
+const client = createClient();
+const RedisStore = connect(session);
 
 app.use(helmet());
 app.use(cors());
 app.use(compression());
 app.use(logger);
-app.use(bodyParser.json());
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: 'secret',
+    store: new RedisStore({ client }),
+  })
+);
+app.use(json());
 app.use(router);
