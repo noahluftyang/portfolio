1f7e330668855200d336ad50a10f6e92ce0d6940
noahluftyang <euihyun.yang.9x@gmail.com>
2020-07-19 18:04:16 +0900

    chore(universal-auth-server): update dependencies

--- services/universal-auth-server/src/app.ts
+++ services/universal-auth-server/src/app.ts
@@ -1,12 +1,13 @@
 import './passport';
 
+import { ExpressOIDC } from '@okta/oidc-middleware';
 import { json } from 'body-parser';
 import * as compression from 'compression';
 // import * as connect from 'connect-redis';
 // import * as cookieParser from 'cookie-parser';
 import * as cors from 'cors';
 import * as express from 'express';
-// import * as session from 'express-session';
+import * as session from 'express-session';
 import * as helmet from 'helmet';
 import * as pino from 'pino-http';
 
@@ -17,18 +18,27 @@ export const app = express();
 
 // const RedisStore = connect(session);
 
+const oidc = new ExpressOIDC({
+  client_id: '{clientId}',
+  client_secret: '{clientSecret}',
+  issuer: 'https://dev-931401-admin.okta.com/oauth2/default',
+  redirect_uri: 'http://localhost:3000/authorization-code/callback',
+  scope: 'openid profile',
+});
+
 app.use(helmet());
 app.use(cors());
 app.use(compression());
 app.use(pino());
 // app.use(cookieParser());
-// app.use(
-//   session({
-//     resave: false,
-//     saveUninitialized: false,
-//     secret: 'secret',
-//     store: new RedisStore({ client: redis }),
-//   })
-// );
+app.use(
+  session({
+    resave: true,
+    saveUninitialized: false,
+    secret: 'secret',
+    // store: new RedisStore({ client: redis }),
+  })
+);
 app.use(json());
 app.use(router);
+app.use(oidc.router);
