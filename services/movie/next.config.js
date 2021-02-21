d10b1cdcd52a3481fe26090e7ff747c65cedcffa
noahluftyang <euihyun.yang.9x@gmail.com>
2021-02-21 12:46:13 +0900

    fix(movie): change into ssg build

--- services/movie/next.config.js
+++ services/movie/next.config.js
@@ -1,11 +1,6 @@
 module.exports = {
-  i18n: {
-    defaultLocale: 'ko-KR',
-    locales: ['en-US', 'ko-KR'],
-  },
   images: {},
   publicRuntimeConfig: {
     TMDB_ACCESS_TOKEN: process.env.TMDB_ACCESS_TOKEN,
   },
-  target: 'serverless',
 };
